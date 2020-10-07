6c3ffd76e95970152f4ef92b0bb34846f63cf316
NandeeshG <nandeeshgupta@gmail.com>
2020-10-07 15:34:32 +0530

    Changed the script to work with a data cache type window. Preloads 10 image window.

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ Scroll through large number of pictures in a folder.
 TODOS - 
 
 1. Add scroll instead of buttons
-2. Add multiple pictures in single window
-3. Use filesystem to get imglimit instead of hardcoding it 
-4. Show speed affects in comparison with loading 500 pictures at once
+2. Increase image loading times
+3. Add multiple pictures in single window
+4. Use filesystem to get imglimit instead of hardcoding it 
+5. Show speed affects in comparison with loading 500 pictures at once
 
 Limitations - 
 All pics should have a welldefined naming scheme with indexes, example pic12.jpg
--- script.js
+++ script.js
@@ -2,70 +2,120 @@
 
 window.onload = function(){
     init();
+    move(1); //one pic displayed
 }
 
-var height,width,imgindex,imglimit
+// TRY SCROLLING LATER
+//var scrollHandle = function(e){
+//    console.log('SCROLLED ',e);
+//
+//    var loc = document.documentElement.scrollTop;
+//    var lastloc = window.scrollY;
+//
+//    console.log('dE ',loc);
+//    console.log('ll ',lastloc);
+//
+//    //if(loc <= 0) moveUp();
+//    if(loc>=11) moveDown();
+//}
+//
+//window.addEventListener('scroll', function(e){scrollHandle(e)});
+
+var height,width
+var imgindex,imglimit
+var data_len,data_index
+var adjust
+var data = []
 
 var init = function(){
     imgindex = 0
     imglimit = 88  //NEED TO HARDCODE THIS RN, Can use filesystem to count number of jpgs in my folder
 
+    height = document.documentElement.clientHeight;
+    width  = document.documentElement.clientWidth;
+    adjust = Number(50)
+    console.log('__Height found - ',height);
+    console.log('__Width found - ',width);
+    console.log('__Adjust length - ',adjust);
+
+    data_len = 10
+    data_index = 0
+    compute_data()
+
     var btn_up = document.getElementById('btn_up');
     var btn_down = document.getElementById('btn_down');
 
-    height = document.documentElement.clientHeight;
-    width  = document.documentElement.clientWidth;
 
-    btn_up.addEventListener('click', moveUp);
-    btn_down.addEventListener('click', moveDown);
+    btn_up.addEventListener('click', function(){move(-1)});
+    btn_down.addEventListener('click', function(){move(1)});
 }
 
-var moveUp = function(){
-    console.log('moving up..');
-
-    // 1. Clear container
-    var container = document.getElementById('img_cont');
-    container.innerHTML = "";
-
-    // 2. Decrease imgindex and create element
-    imgindex--
-
-    var imgpath = getImgPath()
-
-    var newimg = document.createElement('img');
-    newimg.src = imgpath
-    newimg.height = (height)-50;
-
-    // 3. Add to container
-    container.appendChild(newimg);
-}
-
-var moveDown = function(){
+var move = function(delta){
     console.log('moving down..');
 
     // 1. Clear container
     var container = document.getElementById('img_cont');
     container.innerHTML = "";
 
-    // 2. Increase imgindex and create element
-    imgindex++
-
-    var imgpath = getImgPath()
-
-    var newimg = document.createElement('img');
-    newimg.src = imgpath
-    newimg.height = (height)-50;
+    // 2. Increase/Decrease imgindex and get element
+    imgindex += delta
+    if(imgindex<=0) imgindex = 1
+    if(imgindex>imglimit) imgindex=imglimit
+    var newimg = getImage()
 
     // 3. Add to container
     container.appendChild(newimg);
 }
 
-var getImgPath = function(){
-    if(imgindex<=0) imgindex=1
-    if(imgindex>imglimit) imgindex=imglimit
+var getImage = function(){
+    var data_first = data_len*data_index + 1
+    var data_last  = data_len*data_index + data_len
 
-    var imgpath = `./media/pic${imgindex}.jpg`
-    return imgpath
+    console.log('gI_DataLen ',data_len);
+    console.log('gI_DataIndex ',data_index);
+    console.log('gI_Imgindex ',imgindex);
+    console.log('gI_DataFirst ',data_first);
+    console.log('gI_DataLast ',data_last);
+
+    // we moved up
+    if(data_first>imgindex){
+	data_index--;
+	compute_data();
+    }else if(data_last<imgindex){  //we moved down
+	data_index++;
+	compute_data();
+    }
+    if(data_index<0) data_index = 0
+    if(data_index>=(imglimit/10)) data_index=(imglimit/10)-1
+
+    // we are in current data window
+    console.log(data);
+    var ref_index = imgindex%data_len - 1
+    if(ref_index == -1) ref_index = data_len-1;
+    console.log(ref_index);
+    return data[ref_index]
 }
 
+var compute_data = function(){
+
+    data = []
+    var data_first = data_len*data_index + 1
+    var data_last  = data_len*data_index + data_len
+
+    console.log('c_DataLen ',data_len);
+    console.log('c_DataIndex ',data_index);
+    console.log('c_DataFirst ',data_first);
+    console.log('c_DataLast ',data_last);
+
+    for(var i=data_first; i<=Math.min(imglimit,data_last); ++i){
+	var imgpath = `./media/pic${i}.jpg`
+	var newimg = document.createElement('img');
+	//var newimg = new Image();
+	newimg.src = imgpath
+	newimg.height = (height-adjust);
+	data.push(newimg);
+	//data += newimg
+    }
+    console.log('Data',data);
+}
 
